bd24fb0956ace9daacbc3ec48295ecab010197b6
Frank Nkuyahaga <hagafrank@gmail.com>
2013-01-27 14:33:20 +0300

    Backups deleted

--- PIMS Development Version - Backup 27Jan/PIMS Development Version/JavaScriptLibrary/JavaScript/CloseActiveToolTip.js
+++
@@ -1,9 +0,0 @@
-﻿function CloseActiveToolTip() {
-    setTimeout(function () {
-        if (Telerik.Web.UI.RadToolTipController) {
-            var controller = Telerik.Web.UI.RadToolTipController.getInstance();
-            var tooltip = controller.get_activeToolTip();
-            if (tooltip) tooltip.hide();
-        }
-    }, 200);
-}
--- PIMS Development Version - Backup 27Jan/User_Control/Life_Benefit_Application/ApplicationDetails.ascx.vb
+++
@@ -1,5 +0,0 @@
-﻿
-Partial Class User_Control_Life_Benefit_Application_ApplicationDetails
-    Inherits System.Web.UI.UserControl
-
-End Class
